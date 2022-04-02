c7297c7db8ae00d8189a46977726ff635586c34d
Joe Barnett <joe.barnett@cambridge-intelligence.com>
2022-04-02 13:13:29 +0100

    wip

--- keyboards/keebio/iris/keymaps/ensconced/config.h
+++ keyboards/keebio/iris/keymaps/ensconced/config.h
@@ -18,6 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
-#define EE_HANDS
+// #define EE_HANDS
 #define COMBO_COUNT 1
-#define COMBO_TERM 200
+// #define COMBO_TERM 200
